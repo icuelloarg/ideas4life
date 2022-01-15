6dc958490d671c43a36dcd9f14ad9a7792c0db59
icuelloarg <icuelloarg@gmail.com>
2022-01-15 11:30:35 -0300

    New design

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 
-/* 
-const fliper = document.querySelector(".card")
 
-fliper.addEventListener("click", flipCard);
+let plusparagraph1 = document.getElementById("readMore");
+plusparagraph1.onclick = function readMore1(){
+    
+    document.getElementById("plusText").className = "plusParagraph1";
+    plusparagraph1.className = "readMore-1";
 
-function flipCard(){
-    fliper.classList.toggle("flipCard");
-} */
+    let lessParagraph = document.getElementById("readLess");
+lessParagraph.onclick = () => {
+    plusparagraph1.className = "readMore";
+    document.getElementById("plusText").className = "plusParagraph"};
+    
+}
