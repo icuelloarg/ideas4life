671d6bf2c0fe5027210e1dc91a76c83486b87309
icuelloarg <icuelloarg@gmail.com>
2022-01-18 12:09:57 -0300

    Adiciono JS para efectos slide

--- js/index.js
+++ js/index.js
@@ -12,3 +12,42 @@ lessParagraph.onclick = () => {
     document.getElementById("plusText").className = "plusParagraph"};
     
 }
+
+let linkToggle = document.querySelectorAll('.slideOn');
+
+for(i = 0; i < linkToggle.length; i++){
+
+  linkToggle[i].addEventListener('click', function(event){
+
+    event.preventDefault();
+
+    let container = document.getElementById(this.dataset.container);
+
+    if (!container.classList.contains('active')) {
+      
+      container.classList.add('active');
+      container.style.height = 'auto';
+
+      let height = container.clientHeight + 'px';
+
+      container.style.height = '0px';
+
+      setTimeout(function () {
+        container.style.height = height;
+      }, 0);
+      
+    } else {
+      
+      container.style.height = '0px';
+
+      container.addEventListener('transitionend', function () {
+        container.classList.remove('active');
+      }, {
+        once: true
+      });
+      
+    }
+    
+  });
+
+}
